27b86df3d6fff755f33d02a78c150d05834fbcf6
khoihuynh300 <khoihuynh300@gmail.com>
2023-12-14 13:39:49 +0700

    sửa api refresh-token

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -131,7 +131,7 @@ const getDetailsUser = async (req, res) => {
 
 const refreshToken = async (req, res) => {
     try {
-        let token = req.headers.token.split(' ')[1]
+        let token = req.cookies.refresh_token;
         if (!token) {
             return res.status(200).json({
                 status: 'ERR',
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { default: mongoose } = require('mongoose');
 const cors = require("cors");
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 dotenv.config()
 
 const app = express();
@@ -19,7 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(bodyParser.json())
-// app.use(cookieParser());
+app.use(cookieParser());
 
 routes(app);
 
